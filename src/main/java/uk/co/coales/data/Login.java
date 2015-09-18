5bc3f4a16724dc5531300443448cbf97789860f6
joshcoales <joshua@coales.co.uk>
2015-09-18 19:38:32 +0100

    Some placeholder methods for Login class

---
+++ src/main/java/uk/co/coales/data/Login.java
@@ -0,0 +1,40 @@
+package uk.co.coales.data;
+
+import java.util.ArrayList;
+
+public class Login {
+	private String mUsername = null;
+	private String mEmail = null;
+	
+	/**
+	 * Creates a new login from a valid authentication token.
+	 */
+	public static Login fromSessionToken(String authToken) {
+		return null;
+	}
+
+	/**
+	 * Lists diary entries for the current login.
+	 * @return 
+	 */
+	public ArrayList<DiaryEntry> listDiaryEntries() {
+		return null;
+	}
+	
+	/**
+	 * Gets a diary entry by entry id for current login.
+	 */
+	public DiaryEntry getDiaryEntryById(Integer diaryEntryId) {
+		return null;
+	}
+	
+	/**
+	 * Add a new diary entry
+	 * @param entryDate
+	 * @param entryText
+	 * @return
+	 */
+	public DiaryEntry addDiaryEntry(Date entryDate, String entryText) {
+		return null;
+	}
+}
