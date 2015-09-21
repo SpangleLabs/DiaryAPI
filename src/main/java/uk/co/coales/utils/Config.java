4df7743aab9642dc60f36ca25301e5a4a27c19f6
joshcoales <joshua@coales.co.uk>
2015-09-22 00:18:11 +0100

    Reorganising Config class for yaml data loading

--- src/main/java/uk/co/coales/utils/Config.java
+++ src/main/java/uk/co/coales/utils/Config.java
@@ -1,5 +1,13 @@
 package uk.co.coales.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
 public class Config {
 	private String mDbHost = null;
 	private Integer mDbPort = null;
@@ -8,8 +16,29 @@ public class Config {
 	private String mDbPassword = null;
 	private String mKeyDirectory = null;
 	
-	public void loadFromXml() {
-		
+	/**
+	 * Loads configuration information from the yaml config file.
+	 * Gets very upset if the yaml is formatted incorrectly.
+	 */
+	@SuppressWarnings("unchecked")
+	public void loadFromYaml() {
+		InputStream input;
+		try {
+			input = new FileInputStream(new File("src/main/resources/config.yml"));
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: CONFIG FILE MISSING.");
+			e.printStackTrace();
+			return;
+		}
+		Yaml yaml = new Yaml();
+		Map<String,Object> data = (Map<String,Object>) yaml.load(input);
+		Map<String,String> databaseData = (Map<String,String>) data.get("database");
+		this.mDbHost = (String) databaseData.get("host");
+		this.mDbPort = Integer.parseInt(databaseData.get("port"));
+		this.mDbName = (String) databaseData.get("name");
+		this.mDbUsername = (String) databaseData.get("username");
+		this.mDbPassword = (String) databaseData.get("password");
+		this.mKeyDirectory = (String) data.get("key_directory");
 	}
 
 	/**
