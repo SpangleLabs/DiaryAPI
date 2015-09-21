358327016d04d0958687ec380e5aaf8744792c2c
joshcoales <joshua@coales.co.uk>
2015-09-21 08:34:27 +0100

    Large amount of upgrades to LoginService class

--- src/main/java/uk/co/coales/diary/LoginService.java
+++ src/main/java/uk/co/coales/diary/LoginService.java
@@ -1,22 +1,50 @@
 package uk.co.coales.diary;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.codec.binary.Hex;
+
+import uk.co.coales.utils.Database;
+
 @Path("/login")
 public class LoginService {
 
-	@POST
+	@GET
 	@Path("/salt")
-	public Response generateSalt() {
-		String output = "Newsalt";
+	public Response loginSalt() {
+		//Connect to database
+		Database db = new Database();
+		//Generate unique salt
+		String hexSalt = this.generateSalt(db);
+		String output = "Newsalt:"+hexSalt;
 		return Response.status(200).entity(output).build();
 	}
 	
-	@POST
+	/**
+	 * Generates a crypto-secure random salt, converts it to hexadecimal and stores it in the database.
+	 * @return
+	 */
+	private String generateSalt(Database db) {
+		//Generate salt
+		Random r = new SecureRandom();
+		byte[] newSalt = new byte[32];
+		r.nextBytes(newSalt);
+		//Convert to hex
+		String hexSalt = Hex.encodeHexString(newSalt);
+		//Store in database
+		db.addUniqueSalt(hexSalt);
+		return hexSalt;
+	}
+	
+	@GET
 	@Path("/token")
-	public Response login() {
+	public Response loginToken() {
 		String output = "Login session token";
 		return Response.status(200).entity(output).build();
 	}
