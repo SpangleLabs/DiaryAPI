fa650fca0f331158ddb4204d3ba7e47473c03111
joshcoales <joshua@coales.co.uk>
2015-10-01 08:51:01 +0100

    Implemented /entries/yyyy-mm-dd resource

--- src/main/java/uk/co/coales/diary/EntriesService.java
+++ src/main/java/uk/co/coales/diary/EntriesService.java
@@ -1,6 +1,8 @@
 package uk.co.coales.diary;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
@@ -83,4 +85,42 @@ public class EntriesService {
 		return Response.status(200).entity(outputJson).build();
 	}
 
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{entryYear}-{entryMonth}-{entryDay}")
+    public Response viewEntry(@PathParam("entryYear") Integer entryYear, @PathParam("entryMonth") Integer entryMonth, @PathParam("entryDay") Integer entryDay) {
+        //Check auth token and get current login
+        String authToken = this.request.getHeader("Authentication");
+        String ipAddr = this.request.getRemoteAddr();
+        if(authToken == null) {
+            return Response.status(401).entity("ACCESS DENIED").build();
+        }
+        Database db = new Database();
+        Login newLogin = Login.fromSessionToken(db,authToken,ipAddr);
+        if(newLogin == null) {
+            return Response.status(401).entity("ACCESS DENIED").build();
+        }
+        //Attempt to create Date
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(Calendar.DAY_OF_MONTH,entryDay);
+        cal.set(Calendar.MONTH,entryMonth-1);
+        cal.set(Calendar.YEAR,entryYear);
+        Date date = cal.getTime();
+        //Get specified diary entry
+        DiaryEntry diaryEntry = newLogin.getDiaryEntryByDate(date);
+        if(diaryEntry == null) {
+            return Response.status(404).entity("ENTRY NOT FOUND").build();
+        }
+        //Return specified diary entry
+        JSONObject outputJson;
+        try {
+            outputJson = diaryEntry.toJson();
+        } catch (JSONException e) {
+            System.out.println("ERROR: diary entry failed to construct JSON object.");
+            e.printStackTrace();
+            return Response.status(500).entity("FAILED TO CONSTRUCT JSON").build();
+        }
+        return Response.status(200).entity(outputJson).build();
+    }
 }
